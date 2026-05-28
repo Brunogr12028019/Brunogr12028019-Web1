e340e30541b7dcb158722df41f1467b2e9b5992c
Bruno Ramos <brunogr@unipam.edu.br>
2026-05-28 00:31:58 +0000

    Aula do dia 27/05/2026

---
+++ aula-js/aula_03.js
@@ -0,0 +1,12 @@
+/* for (let a=0;a<5;a++){
+    console.log(`Valor de ${a+1}`)
+}
+*/
+
+let resultado = 0;
+let valor = 4;
+
+for (let valor = 4; valor > 0; valor--){
+    resultado = resultado * valor;
+}
+console.log(`O valor do fatorial de $ {valor} é: `)
